4205be7845b78f29f826dbb024d45dba4c9d9a3f
Kiki Sarpong <kikisarpong@ksarp.local>
2024-12-29 21:35:43 -0500

    Created function for loading image file paths

--- vio_workspace/src/vio/include/vio/LoadData.h
+++ vio_workspace/src/vio/include/vio/LoadData.h
@@ -17,11 +17,11 @@ class LoadData{
 
         std::vector<Eigen::MatrixXd> load_homogeneous_matrix(std::string& file_path, std::string name = "none");
         std::vector<double> load_timestamps(std::string& file_path);
+        std::vector<std::string> load_images(std::string& file_path, int number_to_load = -1);
 
     private:
         int row_4 = 4;
         int col_4 = 4;
-
 };
 
 #endif /* _LOAD_DATA_H */
--- vio_workspace/src/vio/src/LoadData.cpp
+++ vio_workspace/src/vio/src/LoadData.cpp
@@ -4,7 +4,9 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <filesystem>
 #include "LoadData.h"
+
 
 // Constructor
 LoadData::LoadData(){};
@@ -106,4 +108,51 @@ std::vector<double> LoadData::load_timestamps(std::string& file_path){
         std::cout << k << "\n";
     }
     return timestamps;
+}
+
+
+/*
+This is a method to load the image file names into a 1D array
+input: file_path (image_directory_path), number_to_load (-1 means load all pictures in folder)
+returns: 1D vector of image file destinations
+Note: The directory should NOT contain any other filetypes other than PNG images.
+*/
+std::vector<std::string> LoadData::load_images(std::string& file_path, int number_to_load){
+    std::vector<std::string> vector_of_image_paths;
+
+    if (!std::filesystem::exists(file_path) && !std::filesystem::is_directory(file_path)){
+        std::cerr << "File " << file_path << " doesn't exist or isn't a directory";
+        return vector_of_image_paths;
+    }
+
+    int file_count = 0;
+    int prefix = 1000000;
+    std::string post_fix = ".png"; // Image is a png file
+    std::string name;
+
+    // Iterate through the files in the directory
+    for(const auto& file: std::filesystem::directory_iterator(file_path)){
+
+        if (!std::filesystem::is_regular_file(file)) {  // Only count regular files (ignore directories)
+            std::cerr << "File type error. Skipping...";
+        }
+
+        // If number_to_load variable is set, return vector
+        if(number_to_load != -1 && number_to_load == file_count){
+            return vector_of_image_paths;
+        }
+
+        // Image names are 000000.png , 0000001.png .... 000100.png, etc
+        name = std::to_string(prefix+file_count);
+        // Remove the '1' at the beginning of th number. 1000001 = 000001
+        name = file_path + name.substr(1,name.length() - 1) + post_fix;  
+        std::cout << name << "\n";
+
+        vector_of_image_paths.emplace_back(name);
+
+
+        file_count +=1;  // Increment file_count
+    }
+
+    return vector_of_image_paths;
 }
